0a9d6bc8f96e979019c2697061046c2dd12214ee
Tatyna <tatynaerkinbek@gmailcom>
2023-11-06 23:11:11 +0600

    leetcode

---
+++ leetcode/ArrayReduceTransformation.ts
@@ -0,0 +1,17 @@
+type Fn = (accum: number, curr: number) => number
+
+function reduce(nums: number[], fn: Fn, init: number): number {
+
+
+    if(nums.length === 0){
+        return init;
+    }
+
+    let result: number = init;
+
+    for(let i = 0; i < nums.length; i++){
+        result = fn(result, nums[i])
+    }
+
+    return result;
+};
