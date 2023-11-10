d2464edaa3163d74ab54ad805d97536f063f70f0
Tatyna <111748254+Tatynaae@users.noreply.github.com>
2023-11-11 00:01:26 +0600

    Leetcode (#3)
    * init
    
    * leetcode-solition
    
    * new leetcode solution
    
    ---------
    
    Co-authored-by: Tatyna <tatynaerkinbek@gmailcom>

---
+++ leetcode/symmetric-tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        def isMirror(left, right):
+            if not left and not right:
+                return True
+            if not left or not right:
+                return False
+
+            return (left.val == right.val) and isMirror(left.left, right.right) and isMirror(left.right, right.left)
+
+        if not root:
+            return True
+
+        return isMirror(root.left, root.right)
+
+
+# Example usage:
+# Construct the binary tree from the given input
+# root1 = TreeNode(1, TreeNode(2, TreeNode(3), TreeNode(4)), TreeNode(2, TreeNode(4), TreeNode(3)))
+# root2 = TreeNode(1, TreeNode(2, None, TreeNode(3)), TreeNode(2, None, TreeNode(3)))
+#
+# # Test cases
+# print(isSymmetric(root1))  # Output: True
+# print(isSymmetric(root2))  # Output: False
